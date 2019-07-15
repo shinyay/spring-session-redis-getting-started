b9f3f1c2efc73f072c205ed54b1d28159950fb5d
shinyay <shinya.com@gmail.com>
2019-07-15 21:23:14 +0900

    Modify Main class name

--- src/main/kotlin/io/pivotal/shinyay/session/BootApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/session/BootApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GettingStartedApplication
+class BootApplication
 
 fun main(args: Array<String>) {
-	runApplication<GettingStartedApplication>(*args)
+	runApplication<BootApplication>(*args)
 }
--- src/test/kotlin/io/pivotal/shinyay/session/BootApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/session/BootApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class GettingStartedApplicationTests {
+class BootApplicationTests {
 
 	@Test
 	fun contextLoads() {
