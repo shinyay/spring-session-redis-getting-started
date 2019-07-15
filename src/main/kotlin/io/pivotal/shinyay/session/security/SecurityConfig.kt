3b99ca336685c77f4e7b2ac72366f5c9c7c26ddd
shinyay <shinya.com@gmail.com>
2019-07-15 22:53:22 +0900

    Add override security config function

---
+++ src/main/kotlin/io/pivotal/shinyay/session/security/SecurityConfig.kt
@@ -0,0 +1,21 @@
+package io.pivotal.shinyay.session.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http?.httpBasic()
+                ?.and()
+                ?.authorizeRequests()
+                ?.antMatchers("/")
+                ?.hasRole("ADMIN")
+                ?.anyRequest()
+                ?.authenticated()
+    }
+}
