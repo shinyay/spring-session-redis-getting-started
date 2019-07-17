7adb0eb29f797fd621b728352a43385d346598fb
shinyay <shinya.com@gmail.com>
2019-07-18 07:33:08 +0900

    Modify nullability check

--- src/main/kotlin/io/pivotal/shinyay/session/security/SecurityConfig.kt
+++ src/main/kotlin/io/pivotal/shinyay/session/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package io.pivotal.shinyay.session.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -20,9 +21,10 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 ?.authenticated()
     }
 
+    @Autowired
     fun configureGlobal(auth: AuthenticationManagerBuilder)
             = auth.inMemoryAuthentication()
             .withUser("admin")
             .password("password")
-            .roles("ADMIN")
+            .roles("ADMIN")!!
 }
