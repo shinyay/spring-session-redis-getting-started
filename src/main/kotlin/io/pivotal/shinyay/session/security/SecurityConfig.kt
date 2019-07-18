ee5734935b349adc888cb6d0852d2febc2d9fec2
shinyay <shinya.com@gmail.com>
2019-07-19 00:13:31 +0900

    Modify authentication with BCryptPasswordEncoder

--- src/main/kotlin/io/pivotal/shinyay/session/security/SecurityConfig.kt
+++ src/main/kotlin/io/pivotal/shinyay/session/security/SecurityConfig.kt
@@ -1,12 +1,15 @@
 package io.pivotal.shinyay.session.security
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
@@ -23,18 +26,11 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 ?.authenticated()
     }
 
-//    @Autowired
-//    fun configureGlobal(auth: AuthenticationManagerBuilder)
-//            = auth.inMemoryAuthentication()
-//            .withUser("admin")
-//            .password("password")
-//            .roles("ADMIN")!!
-
-    @Bean
-    override fun userDetailsService(): UserDetailsService {
-        val users = User.withDefaultPasswordEncoder()
-        val manager = InMemoryUserDetailsManager()
-        manager.createUser(users.username("admin").password("password").roles("ADMIN").build())
-        return manager
-    }
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder)
+            = auth.inMemoryAuthentication()
+            .passwordEncoder(BCryptPasswordEncoder())
+            .withUser("admin")
+            .password(BCryptPasswordEncoder().encode("password"))
+            .roles("ADMIN")!!
 }
